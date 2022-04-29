57b0403644fcd7a1fc50350c2045105c5072ef2e
tg90000 <czegelandras@gmail.com>
2022-04-29 14:46:01 +0200

    debug

--- express_api/index.js
+++ express_api/index.js
@@ -61,7 +61,7 @@ app.use(express.static(path.join(__dirname, 'public')))
     .set('views', path.join(__dirname, 'views'))
     .set('view engine', 'ejs')
     .get('/', ((req, res) => res.render('pages/index')));
-app.use((req, res) => { return res.sendFile(path.join(__dirname,'public/index.html')); });
+//app.use((req, res) => { return res.sendFile(path.join(__dirname,'public/index.html')); });
 
 app.listen(51211, () => {
     console.log('A szerver fut')
